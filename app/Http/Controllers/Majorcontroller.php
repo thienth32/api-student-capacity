7606042cf333218b406928472fe9b9890efc4db5
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-03-08 20:04:56 +0700

    danh sach tai khoan

--- app/Http/Controllers/Majorcontroller.php
+++ app/Http/Controllers/Majorcontroller.php
@@ -10,13 +10,31 @@ class Majorcontroller extends Controller
 
     public function listMajor()
     {
-        $limit = 10;
-        $dataMajor = Major::sort(request('sort') == 'asc' ? 'asc' : 'desc', request('sort_by') ?? null, 'majors')
-            ->search(request('search') ?? null,['name','slug'])
-            ->paginate(request('limit') ?? $limit);
-        return response()->json([
-            'status' => true,
-            'payload' => $dataMajor->toArray()
-        ]);
+        try {
+            $limit = 10;
+            $dataMajor = Major::sort(request('sort') == 'asc' ? 'asc' : 'desc', request('sort_by') ?? null, 'majors')
+                ->search(request('search') ?? null,['name','slug'])
+                ->paginate(request('limit') ?? $limit);
+
+            return response()->json([
+                'status' => true,
+                'payload' => $dataMajor->toArray()
+            ],200);
+
+        }catch(\Throwable $e) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'payload' => 'Máy chủ bị lỗi , liên hệ quản trị viên để khắc phục sự cố  !'
+                ],
+                506
+            );
+        }
+
+    }
+
+    public function store(Request $request)
+    {
+
     }
 }
--- app/Models/Major.php
+++ app/Models/Major.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Services\Builder\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,4 +10,9 @@ class Major extends Model
 {
     use HasFactory;
     protected $table = 'majors';
+
+    public function newEloquentBuilder($query)
+    {
+        return new Builder($query);
+    }
 }
