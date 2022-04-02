930ef690438f45c0f1ecf5b05cbef78f915518aa
thienth32 <64114903+thienth32@users.noreply.github.com>
2022-04-02 10:29:11 +0700

    cập nhật ảnh và thông tin cá nhân ở header

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -584,7 +584,7 @@
                 <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
                     <!--begin::Menu wrapper-->
                     <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="click" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                        <img src="assets/media/avatars/300-1.jpg" alt="user" />
+                        <img src="{{Auth::user()->avatar}}" alt="user" />
                     </div>
                     <!--begin::User account menu-->
                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-primary fw-bold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -593,14 +593,14 @@
                             <div class="menu-content d-flex align-items-center px-3">
                                 <!--begin::Avatar-->
                                 <div class="symbol symbol-50px me-5">
-                                    <img alt="Logo" src="assets/media/avatars/300-1.jpg" />
+                                    <img alt="Logo" src="{{Auth::user()->avatar}}" />
                                 </div>
                                 <!--end::Avatar-->
                                 <!--begin::Username-->
                                 <div class="d-flex flex-column">
-                                    <div class="fw-bolder d-flex align-items-center fs-5">Max Smith
-                                    <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2">Pro</span></div>
-                                    <a href="#" class="fw-bold text-muted text-hover-primary fs-7">max@kt.com</a>
+                                    <div class="fw-bolder d-flex align-items-center fs-5">{{Auth::user()->name}}
+                                    <span class="badge badge-light-success fw-bolder fs-8 px-2 py-1 ms-2"></span></div>
+                                    <a href="#" class="fw-bold text-muted text-hover-primary fs-7">{{Auth::user()->email}}</a>
                                 </div>
                                 <!--end::Username-->
                             </div>
