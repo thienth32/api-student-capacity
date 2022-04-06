4b53b4511bd7fc58500b858ec9af2c016692e955
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-04-06 14:38:06 +0700

    update api contest

--- app/Http/Controllers/Admin/ContestController.php
+++ app/Http/Controllers/Admin/ContestController.php
@@ -45,8 +45,15 @@ class ContestController extends Controller
                 ->with([
                     'major',
                     'teams',
-                    'rounds',
-                    'enterprise'
+                    'rounds' => function ($q) {
+                        return $q->with([
+                            'teams' => function ($q) {
+                                return $q->with('members');
+                            }
+                        ]);
+                    },
+                    'enterprise',
+                    'judges'
                 ])
                 ->withCount('teams');
             // ->paginate(request('limit') ?? 10);
@@ -283,6 +290,7 @@ class ContestController extends Controller
     private function addCollectionApiContest($contest)
     {
         try {
+            dd('judges');
             return $contest->with(['teams' => function ($q) {
                 return $q->withCount('members');
             }, 'rounds' => function ($q) {
