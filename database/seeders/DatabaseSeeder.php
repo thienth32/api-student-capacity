7a50b69e6d3af2eb420b52e5a6dc2781b001fa11
Uy nguyễn <88134149+ngocuy1234@users.noreply.github.com>
2022-05-12 17:43:47 +0700

    Fake thêm ban giám khỏa cho cuộc thi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,12 +18,12 @@ class DatabaseSeeder extends Seeder
         // $this->call(ContestSeeder::class);
         // $this->call(EnterpriseSeeder::class);
         // $this->call(TypeExamSeeder::class);
-        $this->call(RoundSeeder::class);
+        // $this->call(RoundSeeder::class);
         // $this->call(DonorSeeder::class);
         // $this->call(UserTableSeeder::class);
         // $this->call(TeamSeeder::class);
         // $this->call(memberSeeder::class);
-        // $this->call(judgeSeeder::class);
+        $this->call(judgeSeeder::class);
         // $this->call(resultSeeder::class);
         // \App\Models\User::factory(10)->create();
         // \App\Models\Sponsor::factory(10)->create();
