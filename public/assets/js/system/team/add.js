59052e37e1dda55b84b216a07e05c8260f60f71d
nguyenvantrong25112002 <nguyenvantrong25112002@gamil.com>
2022-05-24 10:03:13 +0700

    fixx 1

--- public/assets/js/system/team/add.js
+++ public/assets/js/system/team/add.js
@@ -1,17 +1,23 @@
 $('select[name="contest_id"]').on('change', function() {
     let id = $(this).val();
-    $.ajax({
-        type: "post",
-        url: urlShowContest,
-        data: {
-            id: id
-        },
-        success: function(response) {
-            $("#member").css("display", "block");
-            max_user = response.payload;
-            $('#mesArrayUser').text('Giới hạn chỉ được ' + max_user + ' thành viên !!')
-            userArray = []
-            teamPage.userArray(userArray);
-        }
-    });
+
+    if (id == '') {
+        $("#member").css("display", "none");
+        return;
+    } else {
+        $.ajax({
+            type: "post",
+            url: urlShowContest,
+            data: {
+                id: id
+            },
+            success: function(response) {
+                $("#member").css("display", "block");
+                max_user = response.payload;
+                $('#mesArrayUser').text('Giới hạn chỉ được ' + max_user + ' thành viên !!')
+                userArray = []
+                teamPage.userArray(userArray);
+            }
+        });
+    }
 });
