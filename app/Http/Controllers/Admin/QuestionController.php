a1abb2daae43ebc4e49951e169c0eeb5a0a2d021
nguyenvantrong25112002 <nguyenvantrong25112002@gamil.com>
2022-06-16 09:50:17 +0700

    fix 3

--- app/Http/Controllers/Admin/QuestionController.php
+++ app/Http/Controllers/Admin/QuestionController.php
@@ -70,17 +70,16 @@ class QuestionController extends Controller
     {
         try {
             if (!($questions = $this->getList()->take(10)->get())) return abort(404);
-            return response() -> json([
+            return response()->json([
                 'status' => true,
                 'payload' => $questions,
             ]);
         } catch (\Throwable $th) {
-            return response() -> json([
+            return response()->json([
                 'status' => false,
                 'payload' => 'Hệ thống đã xảy ra lỗi ! ',
-            ],404);
+            ], 404);
         }
-
     }
 
     /**
@@ -353,16 +352,17 @@ class QuestionController extends Controller
             return redirect()->back();
         } catch (\Throwable $th) {
             return abort(404);
+        }
+    }
     public function save_questions(Request $request)
     {
         try {
             $ids = [];
             $exams = Exams::whereId($request->exam_id)->first();
-            foreach($request -> question_ids ?? [] as $question_id)
-            {
-                array_push($ids,(int)$question_id['id']);
+            foreach ($request->question_ids ?? [] as $question_id) {
+                array_push($ids, (int)$question_id['id']);
             }
-            $exams -> questions() -> sync($ids);
+            $exams->questions()->sync($ids);
             return response()->json([
                 'status' => true,
                 'payload' => 'Cập nhật trạng thái thành công  !',
@@ -379,7 +379,7 @@ class QuestionController extends Controller
     {
         try {
             $exams = Exams::whereId($request->exam_id)->first();
-            $exams -> questions()->detach($request->questions_id);
+            $exams->questions()->detach($request->questions_id);
             return response()->json([
                 'status' => true,
                 'payload' => 'Cập nhật trạng thái thành công  !',
