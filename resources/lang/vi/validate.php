b7ee16ca0f80eb765630f2e618b6377494be1e7c
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-07-07 14:58:45 +0700

    fix validate

---
+++ resources/lang/vi/validate.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // Validate
+    "required" => "Chưa nhập trường này !",
+    "unique" => "Trường nhập dữ liệu đã tồn tại trong hệ thống ! ",
+    "max" => "Độ dài ký tự trường này quá lớn ! ",
+    "maxImage" => "Dung lượng trường dữ liệu nhập quá lớn ! ",
+    'mimes' => 'Trường nhập không đúng định dạng !',
+    'date' => 'Trường nhập không đúng định dạng !',
+    'numeric' =>  'Trường nhập định dạng !',
+
+];
