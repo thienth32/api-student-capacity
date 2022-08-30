2d6b05d426b90b6b330ec5befdc17fd3693079b0
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-08-30 10:14:05 +0700

    FIX  Quản lý đề thi cho vòng thi cập nhật thêm trạng thái cho đề, có thể tắt kích hoạt đề thi, chỉ sử dụng những đề thi đang bật

--- public/assets/js/system/exam/exam.js
+++ public/assets/js/system/exam/exam.js
@@ -1,41 +1,54 @@
 const exam = {
-    download_file: function() {
-        $(document).on('click', 'button.download_file', function(e) {
+    download_file: function () {
+        $(document).on("click", "button.download_file", function (e) {
             e.preventDefault();
-            let external_url = $(this).attr('data-external_url');
+            let external_url = $(this).attr("data-external_url");
             Swal.fire({
-                title: 'Bạn có chắc muốn tải tệp xuống ?',
+                title: "Bạn có chắc muốn tải tệp xuống ?",
                 // text: "You won't be able to revert this!",
-                icon: 'question',
+                icon: "question",
                 showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Vâng , tôi đồng ý !',
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Vâng , tôi đồng ý !",
                 cancelButtonText: "Tôi không muốn nữa !",
             }).then((result) => {
                 if (result.isConfirmed) {
-
-                    window.location.href = external_url
-                    toastr.success(response.payload)
+                    window.location.href = external_url;
+                    toastr.success(response.payload);
                 }
-            })
+            });
         });
     },
-    edit_exam: function() {
+    edit_exam: function () {
         function getTimeStamp() {
             var now = new Date();
-            return ((now.getFullYear() + 1) + '-' +
-                ((now.getMonth() < 10) ? ("0" + now.getMonth()) : (now.getMonth())) + '-' +
-                ((now.getDate() < 10) ? ("0" + now.getDate()) : (now.getDate())) + " " +
-                now.getHours() + ':' +
-                ((now.getMinutes() < 10) ? ("0" + now.getMinutes()) : (now.getMinutes())) + ':' +
-                ((now.getSeconds() < 10) ? ("0" + now.getSeconds()) : (now.getSeconds())));
+            return (
+                now.getFullYear() +
+                1 +
+                "-" +
+                (now.getMonth() < 10 ? "0" + now.getMonth() : now.getMonth()) +
+                "-" +
+                (now.getDate() < 10 ? "0" + now.getDate() : now.getDate()) +
+                " " +
+                now.getHours() +
+                ":" +
+                (now.getMinutes() < 10
+                    ? "0" + now.getMinutes()
+                    : now.getMinutes()) +
+                ":" +
+                (now.getSeconds() < 10
+                    ? "0" + now.getSeconds()
+                    : now.getSeconds())
+            );
         }
-        $(document).on('click', 'button.edit_exam', function(e) {
+        $(document).on("click", "button.edit_exam", function (e) {
             e.preventDefault();
-            let href = $(this).attr('data-href');
+            let href = $(this).attr("data-href");
             // let date_time = $(this).attr('data-date_time');
-            const date_time = new Date($(this).attr('data-date_time')).getTime();
+            const date_time = new Date(
+                $(this).attr("data-date_time")
+            ).getTime();
             // const datetimeToday = getTimeStamp();
             const today = new Date().getTime();
             console.log(today);
@@ -45,22 +58,78 @@ const exam = {
             // console.log('Ngày bắt đầu thi  ' + date_time);
             if (today > date_time) {
                 Swal.fire({
-                    title: 'Thời gian thi đã bắt đầu không thể chỉnh sửa đề thi !!',
+                    title: "Thời gian thi đã bắt đầu không thể chỉnh sửa đề thi !!",
                     icon: "warning",
                     buttonsStyling: false,
                     confirmButtonText: "Ok !!",
                     customClass: {
-                        confirmButton: "btn btn-warning "
-                    }
+                        confirmButton: "btn btn-warning ",
+                    },
                 });
-
             } else {
-                window.location.href = href
-
+                window.location.href = href;
             }
         });
-    }
-}
+    },
+
+    selectChangeStatus: function () {
+        $(".form-select-status").on("change", function () {
+            loadTast();
+            let id = $(this).data("id");
+            var that = this;
+            if ($(this).val() == 1) {
+                $.ajax({
+                    url: `${url}/${id}/un-status`,
+                    method: "POST",
+                    data: {
+                        _token: _token,
+                    },
+                    success: function (data) {
+                        if (!data.status) return alert(data.payload);
+                        $(that).val(0);
+                        loadTast(
+                            "Thành công !",
+                            "toastr-bottom-left",
+                            "success"
+                        );
+                    },
+                    error: function (request, status, error) {
+                        loadTast(
+                            "Không thành công !",
+                            "toastr-bottom-left",
+                            "info"
+                        );
+                    },
+                });
+            } else {
+                $.ajax({
+                    url: `${url}/${id}/re-status`,
+                    method: "POST",
+                    data: {
+                        _token: _token,
+                    },
+                    success: function (data) {
+                        if (!data.status) return alert(data.payload);
+                        $(that).val(1);
+                        loadTast(
+                            "Thành công !",
+                            "toastr-bottom-left",
+                            "success"
+                        );
+                    },
+                    error: function (request, status, error) {
+                        loadTast(
+                            "Không thành công !",
+                            "toastr-bottom-left",
+                            "info"
+                        );
+                    },
+                });
+            }
+        });
+    },
+};
 
 exam.download_file();
 exam.edit_exam();
+exam.selectChangeStatus();
