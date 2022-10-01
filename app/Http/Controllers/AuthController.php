c718c48b61b581bab08d31c6a6bfdaa293f7886e
VanQuang <83849198+vanquang202@users.noreply.github.com>
2022-10-01 13:44:28 +0700

    fix login status = 0

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -64,6 +64,12 @@ class AuthController extends Controller
         $user = User::with('roles')->where('email', $googleUser->email)->first();
         if ($user) {
             //            $user->avatar = $googleUser->avatar;
+            if($user->status == 0 ) return response()->json(
+                [
+                    'status' => false,
+                    'payload' => "Xác thực thất bại",
+                ]
+            );
             $user->save();
             $token = $user->createToken('auth_token')->plainTextToken;
             return response()->json([
