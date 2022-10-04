b486eadff70d3c9b184587fd0b4a6b0fac131350
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-10-04 15:59:38 +0700

    fix download đề thi

--- app/Casts/FormatImageGet.php
+++ app/Casts/FormatImageGet.php
@@ -18,7 +18,7 @@ class FormatImageGet implements CastsAttributes
      */
     public function get($model, string $key, $value, array $attributes)
     {
-
+        if (request()->is('admin/*')) return $value;
         if (Storage::disk('s3')->has($value ?? "abc.jpg")) return Storage::disk('s3')->temporaryUrl($value, now()->addDays(7));
         // if (Storage::disk('s3')->has($value ?? "abc.jpg")) return Storage::disk('s3')->temporaryUrl($value, now()->addMinutes(60));
         return ($model->getTable() == 'users') ? $value : null;
