a8f5e5c065e270a214837efe41c602f00b50defb
vanquang202 <83849198+vanquang202@users.noreply.github.com>
2022-10-14 15:27:54 +0700

    fix api show code maanager

--- app/Http/Controllers/Admin/CodeManagerController.php
+++ app/Http/Controllers/Admin/CodeManagerController.php
@@ -507,9 +507,7 @@ class CodeManagerController extends Controller
                 "status" => true,
                 "payload" => $this->challenge->apiShow($id, ['sample_code' => function ($q) {
                     return $q->with(['code_language']);
-                }, 'test_case' => function ($q) {
-                    return $q->where('status', 1);
-                }]),
+                }, 'test_case']),
             ]);
         } catch (\Throwable $th) {
             return response()->json([
