1e6f6501747da706bc2d9785cbea61c9705f4c34
nguyenvantrong25112002 <nguyenvantrong25112002@gmail.com>
2022-11-22 17:05:30 +0700

    fake data team

--- database/seeders/TakeExam_ResultSeeder.php
+++ database/seeders/TakeExam_ResultSeeder.php
@@ -23,11 +23,9 @@ class TakeExam_ResultSeeder extends Seeder
         $qltFake = 10;
         $today = Carbon::now()->format('Y-m-d H:i:s');
         $contests = Contest::where('type', 0)->whereDate('register_deadline', '<', $today)->has('rounds')->get()->pluck(['id']);
-        // $contests = Contest::find(49);
-        $rounds = Round::where('contest_id', $contests->id)->get()->pluck(['id']);
-        $roundTeams = RoundTeam::whereIn('round_id', $rounds)->has('exams')->doesntHave('takeExam')->with(['exams' => function ($q) {
-            return $q->select(['id', 'round_id']);
-        }])->get()->makeHidden(['created_at', 'updated_at', 'deleted_at', 'status'])->random($qltFake)->toArray();
+        $rounds = Round::where('contest_id', $contests->toArray())->get()->pluck(['id']);
+        $roundTeams = RoundTeam::whereIn('round_id', $rounds)->has('exams')->doesntHave('takeExam')
+            ->with(['exams'])->get()->makeHidden(['created_at', 'updated_at', 'deleted_at', 'status'])->random($qltFake)->toArray();
         $resultArr = [];
         $takeExam = [];
         foreach ($roundTeams as $key => $roundTeam) {
@@ -40,16 +38,20 @@ class TakeExam_ResultSeeder extends Seeder
                     'final_point' => rand(5, 10),
                     'result_url' => null,
                     'file_url' => 'https://www.facebook.com/nguyenvantrong2511/',
-                    'status' => 2
+                    'status' => 2,
+                    "created_at" =>  \Carbon\Carbon::now(), # new \Datetime()
+                    "updated_at" => \Carbon\Carbon::now(),  # new \Datetime()
                 ]
             );
             array_push($resultArr, [
                 'team_id' => $roundTeam['team_id'],
                 'round_id' => $roundTeam['round_id'],
-                'point' => rand(3, 10)
+                'point' => rand(3, 10),
+                "created_at" =>  \Carbon\Carbon::now(), # new \Datetime()
+                "updated_at" => \Carbon\Carbon::now(),  # new \Datetime()
             ]);
         }
-        TakeExam::insert($resultArr);
+        TakeExam::insert($takeExam);
         Result::insert($resultArr);
     }
 }
