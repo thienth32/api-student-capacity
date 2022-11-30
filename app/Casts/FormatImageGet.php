99ba50213691269804aa70ad6e1b7d97ecb79352
vanquang202 <kopopi593@gmail.com>
2022-11-30 13:57:25 +0700

    fix download url exam
    fix download url exam

--- app/Casts/FormatImageGet.php
+++ app/Casts/FormatImageGet.php
@@ -7,32 +7,24 @@ use Illuminate\Support\Facades\Storage;
 
 class FormatImageGet implements CastsAttributes
 {
-    /**
-     * Cast the given value.
-     *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
-     * @return mixed
-     */
+
     public function get($model, string $key, $value, array $attributes)
     {
-        if (request()->route()) if (((request()->route()->getName() ?? "ABS") == "admin.round.detail.team.make.exam")) return $value;
+        if (request()->route())
+        {
+            $routeName = (request()->route()->getName() ?? "ABS");
+            $arrayCheckNameRoute = [
+                "admin.round.detail.team.make.exam",
+                "admin.exam.index"
+            ];
+            if(in_array($routeName,$arrayCheckNameRoute)) return $value;
+        };
+
         if (Storage::disk('s3')->has($value ?? "abc.jpg")) return Storage::disk('s3')->temporaryUrl($value, now()->addDays(7));
         // if (Storage::disk('s3')->has($value ?? "abc.jpg")) return Storage::disk('s3')->temporaryUrl($value, now()->addMinutes(60));
         return ($model->getTable() == 'users') ? $value : null;
     }
 
-    /**
-     * Prepare the given value for storage.
-     *
-     * @param  \Illuminate\Database\Eloquent\Model  $model
-     * @param  string  $key
-     * @param  mixed  $value
-     * @param  array  $attributes
-     * @return mixed
-     */
     public function set($model, string $key, $value, array $attributes)
     {
         return $value;
